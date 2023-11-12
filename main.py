a4c59ff729e031fc5d509dc9db4e55e5f4313108
hollop90 <nozolo96@gmail.com>
2023-11-12 22:27:53 +0000

    feat: Implemented mixture function

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ import matplotlib.pyplot as plt
 import time
 import soup
 
-x = soup.demand(range=30)
+"""
+Uncomment for demand plot
+"""
+# x = soup.demand(range=30)
+# vals = [x["Rows"][i]["Value"] for i in range(len(x["Rows"]))] # List comprehension
+# fig, ax = plt.subplots()
+# ax.plot(vals)
+# plt.show()
 
-# print(x["Rows"])
+"""
+Uncomment for mixture plot
+"""
+# x = soup.mixture(range=1)
+# vals = [x["Rows"][i]["Value"] for i in range(len(x["Rows"]))] # List comprehension
+# fig, ax = plt.subplots()
+# ax.pie(vals)
+# plt.show()
 
-vals = [x["Rows"][i]["Value"] for i in range(len(x["Rows"]))]
-fig, ax = plt.subplots()
-ax.plot(vals)
-plt.show()
+"""
+Uncomment for wind gen plot
+"""
+# x = soup.wind(range=30)
+# vals = [x["Rows"][i]["Value"] for i in range(len(x["Rows"]))] # List comprehension
+# fig, ax = plt.subplots()
+# ax.plot(vals)
+# plt.show()
+
 # DATA WE ARE INTERESTED IN
 # - Demand for the month
 # - Fuel mix for the:
--- soup.py
+++ soup.py
@@ -18,12 +18,8 @@ def demand(*, range=None, date_from=None, date_to=None):
         date_to = datetime.date.fromisoformat(date_to)
         date_from = datetime.date.fromisoformat(date_from)
 
-
     range_start = date_from.strftime("%d-%b-%Y")
     range_end = date_to.strftime("%d-%b-%Y")
-
-    print(range_start)
-    print(range_end)
 
     payload = [
         ("area", "demandactual"),
@@ -31,6 +27,33 @@ def demand(*, range=None, date_from=None, date_to=None):
         ("datefrom", f"{range_start} 00:00"),
         ("dateto", f"{range_end} 23:59")
     ]
+    response = requests.get("https://www.smartgriddashboard.com/DashboardService.svc/data", params=payload)
+    if response:
+        return response.json()
+    else:
+        return None
+
+def mixture(*, range=None, date_from=None, date_to=None):
+    range_start = None
+    range_end = None
+
+    if date_from is None and date_to is None:
+        date_to = datetime.date.today()
+        date_from = date_to - datetime.timedelta(days=range)
+    else:
+        date_to = datetime.date.fromisoformat(date_to)
+        date_from = datetime.date.fromisoformat(date_from)
+
+    range_start = date_from.strftime("%d-%b-%Y")
+    range_end = date_to.strftime("%d-%b-%Y")
+
+    payload = [
+        ("area", "fuelmix"),
+        ("region", "ALL"),
+        ("datefrom", f"{range_start} 00:00"),
+        ("dateto", f"{range_start} 23:59")
+    ]
+
     response = requests.get("https://www.smartgriddashboard.com/DashboardService.svc/data", params=payload)
     if response:
         return response.json()
