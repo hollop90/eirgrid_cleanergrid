ed69d5ace42d23906519e0c611f878d88de2ed24
hollop90 <nozolo96@gmail.com>
2023-11-16 20:33:49 +0000

    refactor: DRY up soup. Extract API call to function

--- main.py
+++ main.py
@@ -2,47 +2,12 @@ import matplotlib.pyplot as plt
 import time
 import soup
 
-"""
-Uncomment for demand plot
-"""
-x = soup.demand(date_range=3)
-fig, ax = plt.subplots()
-print(x)
-ax.plot(x["Demand"])
-plt.show()
+demand_data = soup.demand(date_range=3)
+mix_data = soup.mixture(date_range=3)
+wind = soup.wind(date_range=3)
+wind_forecast = soup.wind_forecast(date_range=3)
 
-"""
-Uncomment for mixture plot
-"""
-x = soup.mixture(date_range=3)
-fig, ax = plt.subplots()
-print(x)
-ax.pie(x["Mixture"])
-plt.show()
-
-"""
-Uncomment for wind gen plot
-"""
-x = soup.wind(date_range=3)
-fig, ax = plt.subplots()
-print(x)
-ax.plot(x["Wind"])
-plt.show()
-
-"""
-Uncomment for wind forecast plot
-"""
-x = soup.wind_forecast(date_range=3)
-fig, ax = plt.subplots()
-print(x)
-ax.plot(x["Forecast"])
-plt.show()
-
-# DATA WE ARE INTERESTED IN
-# - Demand for the month
-# - Fuel mix for the:
-#   - day
-#   - week
-#   - month
-# - Wind generation for the month
-# - Wind generation forecast
+print(demand_data)
+print(mix_data)
+print(wind)
+print(wind_forecast)
--- soup.py
+++ soup.py
@@ -3,8 +3,7 @@ import json
 import pandas as pd
 import datetime
 
-endpoint = "https://www.smartgriddashboard.com/DashboardService.svc/data"
-def demand(*, date_range=None, date_from=None, date_to=None):
+def query_dashbaord(area, date_range, date_from, date_to):
     range_start = None
     range_end = None
 
@@ -19,114 +18,61 @@ def demand(*, date_range=None, date_from=None, date_to=None):
     range_end = date_to.strftime("%d-%b-%Y")
 
     payload = [
-        ("area", "demandactual"),
+        ("area", f"{area}"),
         ("region", "ALL"),
         ("datefrom", f"{range_start} 00:00"),
         ("dateto", f"{range_end} 23:59")
     ]
-    response = requests.get(endpoint, params=payload)
+
+    endpoint = "https://www.smartgriddashboard.com/DashboardService.svc/data"
+    return requests.get(endpoint, params=payload)
+
+def demand(*, date_range=None, date_from=None, date_to=None):
+    response = query_dashbaord("demandactual", date_range, date_from, date_to)
+
     if response:
         data = response.json()
-        rows = [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))]
-        dict = {
-            "Demand" : rows
-        }
-        demand = pd.DataFrame(dict)
-        return demand
+        return pd.DataFrame({
+            "Time" : [data["Rows"][i]["EffectiveTime"] for i in range(len(data["Rows"]))],
+            "Demand Actual" : [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))],
+        })
     else:
         return None
 
 def mixture(*, date_range=None, date_from=None, date_to=None):
-    range_start = None
-    range_end = None
+    response = query_dashbaord("fuelmix", date_range, date_from, date_to)
 
-    if date_from is None and date_to is None:
-        date_to = datetime.date.today()
-        date_from = date_to - datetime.timedelta(days=date_range)
-    else:
-        date_to = datetime.date.fromisoformat(date_to)
-        date_from = datetime.date.fromisoformat(date_from)
-
-    range_start = date_from.strftime("%d-%b-%Y")
-    range_end = date_to.strftime("%d-%b-%Y")
-
-    payload = [
-        ("area", "fuelmix"),
-        ("region", "ALL"),
-        ("datefrom", f"{range_start} 00:00"),
-        ("dateto", f"{range_start} 23:59")
-    ]
-
-    response = requests.get(endpoint, params=payload)
     if response:
         # List comprehension
         data = response.json()
-        rows = [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))]
-        dict = {
-            "Mixture" : rows
-        }
-        return pd.DataFrame(dict)
+        return  pd.DataFrame({
+            "Time" : [data["Rows"][i]["EffectiveTime"] for i in range(len(data["Rows"]))],
+            "Source" : [data["Rows"][i]["FieldName"] for i in range(len(data["Rows"]))],
+            "Mixture" : [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))],
+        })
     else:
         return None
 
 def wind(*, date_range=None, date_from=None, date_to=None):
-    range_start = None
-    range_end = None
+    response = query_dashbaord("windactual", date_range, date_from, date_to)
 
-    if date_from is None and date_to is None:
-        date_to = datetime.date.today()
-        date_from = date_to - datetime.timedelta(days=date_range)
-    else:
-        date_to = datetime.date.fromisoformat(date_to)
-        date_from = datetime.date.fromisoformat(date_from)
-
-    range_start = date_from.strftime("%d-%b-%Y")
-    range_end = date_to.strftime("%d-%b-%Y")
-
-    payload = [
-        ("area", "windactual"),
-        ("region", "ALL"),
-        ("datefrom", f"{range_start} 00:00"),
-        ("dateto", f"{range_end} 23:59")
-    ]
-    response = requests.get(endpoint, params=payload)
     if response:
         data = response.json()
-        rows = [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))]
-        dict = {
-            "Wind" : rows
-        }
-        return pd.DataFrame(dict)
+        return  pd.DataFrame({
+            "Time" : [data["Rows"][i]["EffectiveTime"] for i in range(len(data["Rows"]))],
+            "Wind" : [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))],
+        })
     else:
         return None
 
 def wind_forecast(*, date_range=None, date_from=None, date_to=None):
-    range_start = None
-    range_end = None
+    response = query_dashbaord("windforecast", date_range, date_from, date_to)
 
-    if date_from is None and date_to is None:
-        date_to = datetime.date.today()
-        date_from = date_to - datetime.timedelta(days=date_range)
-    else:
-        date_to = datetime.date.fromisoformat(date_to)
-        date_from = datetime.date.fromisoformat(date_from)
-
-    range_start = date_from.strftime("%d-%b-%Y")
-    range_end = date_to.strftime("%d-%b-%Y")
-
-    payload = [
-        ("area", "windforecast"),
-        ("region", "ALL"),
-        ("datefrom", f"{range_start} 00:00"),
-        ("dateto", f"{range_end} 23:59")
-    ]
-    response = requests.get(endpoint, params=payload)
     if response:
         data = response.json()
-        rows = [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))]
-        dict = {
-            "Forecast" : rows
-        }
-        return pd.DataFrame(dict)
+        return pd.DataFrame({
+            "Time" : [data["Rows"][i]["EffectiveTime"] for i in range(len(data["Rows"]))],
+            "Forecast" : [data["Rows"][i]["Value"] for i in range(len(data["Rows"]))],
+        })
     else:
         return None
